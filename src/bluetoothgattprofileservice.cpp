ce63d3550eaa9a8425c8419bd79c4def7325b0f8
Ashish Patel <ashish.patel@lgepartner.com>
2020-04-21 12:58:33 +0530

    Fix the Coverity issues
    :Release Notes:
    Fix the Coverity issues
    
    :Detailed Notes:
    Fix the Coverity issues
    
    :Testing Performed:
    Build successfully
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [CID-127607] Uninitialized scalar variable
    
    Change-Id: I3610512bf9b72fc10bd111d23bd579ba8340a2e0

--- src/bluetoothgattprofileservice.cpp
+++ src/bluetoothgattprofileservice.cpp
@@ -2063,6 +2063,7 @@ bool BluetoothGattProfileService::monitorCharacteristic(LSMessage &message)
 
 		subscriptionInfo.serviceUuid = serviceUuid;
 		subscriptionInfo.characteristicUuid = characteristicUuid;
+		subscriptionInfo.handle = 0;
 	}
 
 	//Set a callback to see if client dropped. We do it this way because we first need to get the client watch object and pass this
