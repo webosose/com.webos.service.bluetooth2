cc114bb6f02ce71794fcc8f99f4e9eaa2bf5fd69
sameer.mulla <sameer.mulla@lge.com>
2020-04-27 11:04:13 +0530

    Fix adapter state is not updating properly
    :Release Notes:
    Fix adapter state is not updating properly BT dongle is removed
    
    :Detailed Notes:
    When BT dongle is removed then list adapters is remaining same,
    only removed adapter state is changed to powered off.
    This commit updating adapters when adapter is removed.
    
    :Testing Performed:
    Builded and Tested
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [PLAT-102335] Test and fix hfp/getStatus for multiple adapter
    
    Change-Id: I25b6de44c17427c41f5db0f9680c3f8f317b4f83

--- src/bluetoothmanagerservice.cpp
+++ src/bluetoothmanagerservice.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2019 LG Electronics, Inc.
+// Copyright (c) 2014-2020 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -460,6 +460,7 @@ void BluetoothManagerService::adaptersChanged()
 			BT_INFO("MANAGER_SERVICE", 0, "adaptersChanged erasing adapter [%s] from list", it->first.c_str());
 			delete it->second;
 			it = mAdaptersInfo.erase(it);
+			notifySubscribersAboutStateChange();
 		}
 		else
 		{
