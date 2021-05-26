d724ed8e39c2f4bebe3177db4670e302e65b5e1b
shoyeb.khan <shoyeb.khan@lge.com>
2021-05-26 18:47:17 +0530

    Address Coverity issues
    :Release Notes:
    Address Coverity issues
    
    :Detailed Notes:
    Addressed Coverity issues
    
    :Testing Performed:
    Built & tested
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [PLAT-140320] Address Coverity issues for
                  bluedroid-mtk & nettools
    [CID-6515555] An identifier with external linkage shall
                  have exactly one external definition
    
    Change-Id: I53e1200045f74a403724ad3e20733376b48c008e
    (cherry picked from commit cbb957d6c84aa6b2f50feefa40fdca4f5cb7ca57)

--- src/clientwatch.h
+++ src/clientwatch.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2020 LG Electronics, Inc.
+// Copyright (c) 2014-2021 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -44,7 +44,6 @@ private:
 	guint mNotificationTimeout;
 
 	void startWatching();
-	void cleanup();
 	void triggerClientDroppedNotification();
 
 	void notifyClientDisconnected();
